74b3ef96e85829efb3841448609595f47fd3f3f0
Heiko Braun <ike.braun@googlemail.com>
2023-08-30 09:14:40 +0000

    remove py file form top level. noticed that I accidentally left a token.

--- fetch_issues.py
+++
@@ -1,6 +0,0 @@
-from github import Github
-token = Github('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')
-repo = token.get_repo('quarkusio/quarkus')
-issues = repo.get_issues(state='open')
-for issue in issues:
-    print(issue.labels)
